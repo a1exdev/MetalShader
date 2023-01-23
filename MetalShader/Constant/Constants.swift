b5914843911cc75b220c5966f4288c7a66f9aeca
Alex Senin <alexsenin@internet.ru>
2023-01-23 14:02:23 +0300

    Shader constants added

---
+++ MetalShader/Constant/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  MetalShader
+//
+//  Created by Alexander Senin on 23.01.2023.
+//
+
+import simd
+
+enum FunctionNames: String {
+    case fragmentShader = "fragment_shader"
+    case vertexShader = "vertex_shader"
+    case texturedFragment = "textured_fragment"
+}
