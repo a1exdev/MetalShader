69bc7130f405328640a1b57a815c467328e6cb2d
Alex Senin <alexsenin@internet.ru>
2023-01-24 15:36:54 +0300

    Matrix constants added

--- MetalShader/Constant/Constants.swift
+++ MetalShader/Constant/Constants.swift
@@ -7,6 +7,14 @@
 
 import simd
 
+struct ModelConstants {
+    var modelViewMatrix = matrix_identity_float4x4
+}
+
+struct SceneConstants {
+    var projectionMatrix = matrix_identity_float4x4
+}
+
 enum FunctionNames: String {
     case fragmentShader = "fragment_shader"
     case vertexShader = "vertex_shader"
